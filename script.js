a796d6a55bc0ccb7697b6bc504a6a4439e9c3c36
SuperDaan0608 <daan09139@gmail.com>
2026-07-09 14:11:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,17 @@ document
 
 
 
-
 const observer =
 new IntersectionObserver(entries=>{
 
 
 entries.forEach(entry=>{
 
-
 if(entry.isIntersecting){
 
 entry.target.classList.add("show");
 
 }
-
 
 });
 
@@ -33,3 +30,63 @@ entry.target.classList.add("show");
 document
 .querySelectorAll(".hidden")
 .forEach(el=>observer.observe(el));
+
+
+
+
+
+function openProject(project){
+
+
+let title =
+document.getElementById("popup-title");
+
+let text =
+document.getElementById("popup-text");
+
+
+
+title.innerHTML = project;
+
+
+
+if(project === "Military System"){
+
+text.innerHTML =
+"Military system with teams, ranks, security systems and administration tools.";
+
+}
+
+
+
+if(project === "Simulator Framework"){
+
+text.innerHTML =
+"Simulator framework including coins, upgrades, shops, leaderboards and saving.";
+
+}
+
+
+
+if(project === "UI System"){
+
+text.innerHTML =
+"Modern Roblox interfaces with animations, menus and notifications.";
+
+}
+
+
+
+document.getElementById("popup").style.display="flex";
+
+
+}
+
+
+
+
+function closeProject(){
+
+document.getElementById("popup").style.display="none";
+
+}
